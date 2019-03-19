d4007a5ac52a7697efdaad6d333d18a777a27a42
Sam <super8989@gmail.com>
2019-03-18 21:30:30 -0400

    component scaffolding

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { MemoryRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 const PageOne = () => {
   return (
@@ -23,12 +23,12 @@ const PageTwo = () => {
 const App = () => {
   return (
     <div>
-      <MemoryRouter>
+      <BrowserRouter>
         <div>
           <Route path="/" exact component={PageOne} />
           <Route path="/pagetwo" component={PageTwo} />
         </div>
-      </MemoryRouter>
+      </BrowserRouter>
     </div>
   );
 };
