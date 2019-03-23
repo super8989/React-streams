95e7655cccf0bc5b61354f15cad65ea9d036c8ad
Sam <super8989@gmail.com>
2019-03-23 15:32:48 -0400

    automatically handling events

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,11 +2,17 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
+  renderInput({ input }) {
+    return <input {...input} /> 
+    //{...formProps.input} take all key value pairs and object from .input and add them as props to input element
+      //<input onChange={formProps.input.onChange} value={formProps.input.value} />
+  }
+
   render() {
     return (
       <form>
-        <Field name="title" />
-        <Field name="description" />
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
       </form>
     );
   }
