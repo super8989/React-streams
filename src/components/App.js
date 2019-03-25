b771211def18b8f7d3f471ea19074df12083dfc4
Sam <super8989@gmail.com>
2019-03-24 21:12:57 -0400

    wildcard navigation

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,7 @@ const App = () => {
           <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </div>
@@ -26,3 +26,5 @@ const App = () => {
 };
 
 export default App;
+
+//:id -> : turns id into a variable
